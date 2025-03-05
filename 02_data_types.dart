d6c26d35a63675e5308dc229d7ae6a731534b5ba
Ramadhan Yoga <ramayoga04@gmail.com>
2025-03-05 20:39:24 +0700

    upload file-file awal belajar dart.

---
+++ 02_data_types.dart
@@ -0,0 +1,16 @@
+void main() {
+  int score = 23;
+  var count = 23;
+
+  double percentage = 93.4;
+  var percent = 82.53;
+
+  String name = "Dhan";
+  var company = "Google";
+
+  bool isValid = true;
+  var isAlive = false;
+
+  print(score);
+  print(percent);
+}
---
+++ 03_string_interpolation.dart
@@ -0,0 +1,14 @@
+void main() {
+  String teks1 = 'Single';
+  String teks2 = "Double";
+  String teks3 = 'It\'s easy';
+  String teks4 = "It's easy";
+
+  String name = "Kevin";
+  print("Hello $name!");
+  print("$name have ${name.length} characters");
+
+  int num1 = 20;
+  int num2 = 10;
+  print("The sum of $num1 and $num2 is ${num1 + num2}");
+}
